1530e536ffc798399f5f2586cd22d3b520300598
Luis Schweigard <luis.schweigard@gmail.com>
2022-08-12 12:33:49 +0000

    Adding option for kubeconfig copy

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "k8s-master-ipv4" {
-  value = "${hcloud_server.k8s-master.ipv4_address}"
+  value = "ssh root@${hcloud_server.k8s-master.ipv4_address}"
+}
+
+output "scp-kubeconfig" {
+  value = "scp root@${hcloud_server.k8s-master.ipv4_address}:~/.kube/config ~/.kube/config && sed -i 's/127.0.0.1/${hcloud_server.k8s-master.ipv4_address}/g' ~/.kube/config"
 }
